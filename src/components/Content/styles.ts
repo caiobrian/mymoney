f6598804891099a6e244e12f24b55a34cbf099f5
Caio <cgomes@doris.mobi>
2020-10-03 20:05:36 -0300

    feat: add new scrollbar style

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -4,4 +4,21 @@ export const Container = styled.div`
   grid-area: CONTENT;
   background-color: ${({ theme }) => theme.colors.primary};
   padding: 25px;
+
+  height: calc(100vh - 70px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.secondary};
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb-track {
+    background-color: ${({ theme }) => theme.colors.tertiary};
+    border-radius: 10px;
+  }
 `;
