de9892782c90091391895bcefb1b600051934fb4
Caio <cgomes@doris.mobi>
2020-10-04 16:56:27 -0300

    fix: rename props and includes new one

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -13,7 +13,7 @@ declare module "styled-components" {
       black: string;
       gray: string;
 
-      sucess: string;
+      success: string;
       info: string;
       warning: string;
     };
